03aa3fe7195bdb283698ca90b78f23bb5afbeb4b
Christopher Miranda <mirandab@gmail.com>
2023-05-08 18:26:13 -0500

    Segundo commit

--- main.c
+++ main.c
@@ -4,19 +4,19 @@
 //caso 3: Calcular el factorial de un número
 int main(void){
 
-    //variables del menú
+    //variables del genereales
     int selector;
     //variables para caso 1
-    int num1, num2, flag, aux;
-    int sumaPrimos, sumaImpares;
+    int aux1, num1, num2, sumaPrimos, sumaImpares;
     int cont, contPrimos, contAuxPrimos, contImpares;
     //variables para caso 2
-    
+    int num, productDigitos, sumDigitos, digitosPares, digitosImpares;
+    int aux2, digit;
 
     do{
         printf("\t---MENÚ---\t\n");
-        printf("1. Opción A\n");
-        printf("2. Opción B\n");
+        printf("1. Impares y primos en un rango\n");
+        printf("2. Trabajo con dígitos de un número\n");
         printf("3. Opción C\n");
         printf("4. Salir\n");
         printf("Ingrese una opción: ");
@@ -25,6 +25,13 @@ int main(void){
         switch(selector)
         {
         case 1:                 //Suma de impares y primos. Cantidad de impares y primos en un rango [a, b] donde a < b
+
+            aux1 = num1;
+            sumaImpares = 0;
+            sumaPrimos = 0;
+            contImpares = 0;
+            contPrimos = 0;
+
             printf("\t--CONTADOR DE PRIMOS E IMPARES, SUMA DE ESTOS\t\n");
 
             printf("Ingrese un número \'A\': ");
@@ -41,34 +48,28 @@ int main(void){
             }
 
             //comprobar primos e impares.
-            aux = num1;
-            sumaImpares = 0;
-            sumaPrimos = 0;
-            contImpares = 0;
-            contPrimos = 0;
-
-            while(aux <= num2){
+            while(aux1 <= num2){
 
                 cont = 1;
                 contAuxPrimos = 0;
-                while (aux >= cont){
+                while (aux1 >= cont){
 
-                    if(aux % cont == 0)
+                    if(aux1 % cont == 0)
                         contAuxPrimos++;
                     cont++;
                 }
                 if(contAuxPrimos == 2){
                     contPrimos++;
-                    sumaPrimos += aux;
+                    sumaPrimos += aux1;
                 }
 
-                if(aux % 2 != 0){
+                if(aux1 % 2 != 0){
 
                     contImpares++;
-                    sumaImpares += aux;
+                    sumaImpares += aux1;
                 } 
                 
-                aux++;
+                aux1++;
             }
 
             printf("En [%d, %d] hay %d números primos\n", num1, num2, contPrimos);
@@ -80,7 +81,36 @@ int main(void){
             break;
         case 2:                 //Producto y suma de dígitos de un número. Cuantos dígitos son pares e impares
 
+            digit = 0;
+            digitosImpares = 0;
+            digitosPares = 0;
+            sumDigitos = 0;
+            productDigitos = 1;
 
+            printf("\t--SUMA Y PRODUCTO DE DÍGITOS DE UN NÚMERO. CUANTOS SON PARES E IMPARES---\t\n");
+
+            do{
+                printf("Ingrese un número entero positivo: ");
+                scanf("%d", &num);
+            }while(num < 0);
+
+            aux2 = num;
+            while(aux2 > 0){
+                digit = aux2 % 10;
+                aux2 /= 10;
+                productDigitos *= digit; //Sumar lo digitos
+                sumDigitos += digit;     //Multiplicar los dígitos
+
+                //Comprobar si el dígito es par o impar                              
+                if(digit % 2 == 0 && digit != 0)
+                    digitosPares++;
+                else if(digit % 2 != 0 && digit != 0)
+                    digitosImpares++;
+            }
+            printf("La suma de los dígitos de %d es: %d\n", num, sumDigitos);            
+            printf("El producto de los dígitos de %d es: %d\n", num, productDigitos);            
+            printf("%d tiene %d dígitos pares\n", num, digitosPares);            
+            printf("%d tiene %d dígitos impares\n", num, digitosImpares);            
             break;
         case 3:                 //Calcular el factorial de un número
             
